7617c5d8ee6835592aefa17adeea70eeac9aa28b
danielbintar <daniel.bintar@bukalapak.com>
2021-05-30 20:50:27 +0700

    publish (#3)

--- pkg/publish.go
+++ pkg/publish.go
@@ -4,6 +4,11 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Shopify/sarama"
 )
 
 //go:generate mockgen -source=publish.go -destination=mock/publish.go
@@ -24,4 +29,110 @@ func StoreEvent(ctx context.Context, db DB, data EventData) error {
 	}
 
 	return nil
+}
+
+type KafkaConsumer struct {
+	Batch     uint
+	DB        *sql.DB
+	Kafka     sarama.SyncProducer
+	WorkerNum uint
+
+	stop bool
+}
+
+func (this *KafkaConsumer) Run() {
+	for i := uint(1); i <= this.WorkerNum; i++ {
+		go func() {
+			for !this.stop {
+				if err := this.Process(); err != nil {
+					time.Sleep(2 * time.Minute)
+				}
+			}
+		}()
+	}
+
+	for !this.stop {
+	}
+}
+
+func (this *KafkaConsumer) Shutdown() {
+	this.stop = true
+}
+
+func (this *KafkaConsumer) Process() error {
+	tx, err := this.DB.Begin()
+	if err != nil {
+		return err
+	}
+
+	if this.Batch < 2 {
+		row := tx.QueryRow("SELECT id, topic, payload FROM events LIMIT 1 FOR UPDATE SKIP LOCKED")
+
+		var id int
+		var topic string
+		var payload []byte
+		if err := row.Scan(&id, &topic, &payload); err != nil {
+			tx.Rollback()
+			return err
+		}
+
+		_, err = tx.ExecContext(context.Background(), "DELETE FROM kafka_events where id = ?", id)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+
+		msg := &sarama.ProducerMessage{
+			Topic: topic,
+			Value: sarama.ByteEncoder(payload),
+		}
+		_, _, err = this.Kafka.SendMessage(msg)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+	} else {
+		rows, err := tx.Query(fmt.Sprintf("SELECT id, topic, payload FROM events LIMIT %d FOR UPDATE SKIP LOCKED", this.Batch))
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+
+		idAry := make([]string, 0)
+		messages := make([]*sarama.ProducerMessage, 0)
+
+		for rows.Next() {
+			var id int
+			var topic string
+			var payload []byte
+			if err := rows.Scan(&id, &topic, &payload); err != nil {
+				tx.Rollback()
+				return err
+			}
+
+			idAry = append(idAry, strconv.Itoa(id))
+			messages = append(messages, &sarama.ProducerMessage{
+				Topic: topic,
+				Value: sarama.ByteEncoder(payload),
+			})
+		}
+		if err := rows.Err(); err != nil {
+			tx.Rollback()
+			return err
+		}
+
+		ids := strings.Join(idAry, "','")
+		_, err = tx.ExecContext(context.Background(), fmt.Sprintf(`DELETE FROM kafka_events where id IN ('%s')`, ids))
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+
+		if err = this.Kafka.SendMessages(messages); err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+
+	return tx.Commit()
 }
--- pkg/publish_test.go
+++ pkg/publish_test.go
@@ -5,10 +5,13 @@ import (
 	"errors"
 	"testing"
 
+	"github.com/pursuit/event-go/mock/shopify/sarama"
 	"github.com/pursuit/event-go/pkg"
 	"github.com/pursuit/event-go/pkg/mock"
 
 	"github.com/golang/mock/gomock"
+
+	"github.com/DATA-DOG/go-sqlmock"
 )
 
 func TestStoreEvent(t *testing.T) {
@@ -43,5 +46,232 @@ func TestStoreEvent(t *testing.T) {
 			(err != nil && testcase.outputErr.Error() != err.Error()) {
 			t.Errorf("Test %s, err is %v, should be %v", testcase.tName, err, testcase.outputErr)
 		}
+	}
+}
+
+func TestKafkaConsumer(t *testing.T) {
+	id := 2
+	topic := "topic1"
+	payload := []byte(`{"foo":"bar"}`)
+
+	for _, testcase := range []struct {
+		tName     string
+		txErr     error
+		queryErr  error
+		deleteErr error
+		kafkaErr  error
+		commitErr error
+		outputErr error
+	}{
+		{
+			tName:     "fail start transaction",
+			txErr:     errors.New("fail"),
+			outputErr: errors.New("fail"),
+		},
+		{
+			tName:     "fail query",
+			queryErr:  errors.New("fail"),
+			outputErr: errors.New("fail"),
+		},
+		{
+			tName:     "fail delete",
+			deleteErr: errors.New("fail"),
+			outputErr: errors.New("fail"),
+		},
+		{
+			tName:     "fail kafka",
+			kafkaErr:  errors.New("fail"),
+			outputErr: errors.New("fail"),
+		},
+		{
+			tName:     "fail commit",
+			commitErr: errors.New("fail"),
+			outputErr: errors.New("fail"),
+		},
+		{
+			tName: "success",
+		},
+		{
+			tName: "batch failed to query",
+		},
+	} {
+		t.Run(testcase.tName, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			kafka := mock_sarama.NewMockSyncProducer(ctrl)
+
+			db, mock, err := sqlmock.New()
+			if err != nil {
+				panic(err)
+			}
+			defer db.Close()
+
+			if testcase.txErr != nil {
+				mock.ExpectBegin().WillReturnError(testcase.txErr)
+			} else {
+				mock.ExpectBegin()
+				if testcase.queryErr != nil {
+					mock.ExpectQuery("SELECT id, topic, payload FROM events LIMIT 1 FOR UPDATE SKIP LOCKED").WillReturnError(testcase.queryErr)
+					mock.ExpectRollback()
+				} else {
+					mock.ExpectQuery("SELECT id, topic, payload FROM events LIMIT 1 FOR UPDATE SKIP LOCKED").WillReturnRows(mock.NewRows([]string{"id", "topic", "payload"}).AddRow(id, topic, payload))
+					if testcase.deleteErr != nil {
+						mock.ExpectExec("DELETE FROM kafka_events where id = ?").WillReturnError(testcase.deleteErr)
+						mock.ExpectRollback()
+					} else {
+						mock.ExpectExec("DELETE FROM kafka_events where id = ?").WillReturnResult(sqlmock.NewResult(1, 1))
+						if testcase.kafkaErr != nil {
+							kafka.EXPECT().SendMessage(gomock.Any()).Return(int32(1), int64(2), testcase.kafkaErr)
+							mock.ExpectRollback()
+						} else {
+							kafka.EXPECT().SendMessage(gomock.Any()).Return(int32(1), int64(2), nil)
+							if testcase.commitErr != nil {
+								mock.ExpectCommit().WillReturnError(testcase.commitErr)
+							} else {
+								mock.ExpectCommit()
+							}
+						}
+					}
+				}
+			}
+
+			consumer := pkg.KafkaConsumer{
+				DB:        db,
+				Kafka:     kafka,
+				Batch:     1,
+				WorkerNum: 1,
+			}
+
+			err = consumer.Process()
+			if (testcase.outputErr == nil && err != nil) ||
+				(testcase.outputErr != nil && err == nil) ||
+				(err != nil && testcase.outputErr.Error() != err.Error()) {
+				t.Errorf("Test %s, err is %v, should be %v", testcase.tName, err, testcase.outputErr)
+			}
+
+			consumer.Shutdown()
+			consumer.Run()
+
+			if err := mock.ExpectationsWereMet(); err != nil {
+				panic(err)
+			}
+		})
+	}
+}
+
+func TestKafkaConsumerBatch(t *testing.T) {
+	id := 2
+	topic := "topic1"
+	payload := []byte(`{"foo":"bar"}`)
+
+	for _, testcase := range []struct {
+		tName     string
+		txErr     error
+		queryErr  error
+		deleteErr error
+		kafkaErr  error
+		commitErr error
+		outputErr error
+	}{
+		{
+			tName:     "fail start transaction",
+			txErr:     errors.New("fail"),
+			outputErr: errors.New("fail"),
+		},
+		{
+			tName:     "fail query",
+			queryErr:  errors.New("fail"),
+			outputErr: errors.New("fail"),
+		},
+		{
+			tName:     "fail scan",
+			outputErr: errors.New(`sql: Scan error on column index 0, name "id": converting driver.Value type string ("invalid id") to a int: invalid syntax`),
+		},
+		{
+			tName:     "fail delete",
+			deleteErr: errors.New("fail"),
+			outputErr: errors.New("fail"),
+		},
+		{
+			tName:     "fail kafka",
+			kafkaErr:  errors.New("fail"),
+			outputErr: errors.New("fail"),
+		},
+		{
+			tName:     "fail commit",
+			commitErr: errors.New("fail"),
+			outputErr: errors.New("fail"),
+		},
+		{
+			tName: "success",
+		},
+	} {
+		t.Run(testcase.tName, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			kafka := mock_sarama.NewMockSyncProducer(ctrl)
+
+			db, mock, err := sqlmock.New()
+			if err != nil {
+				panic(err)
+			}
+			defer db.Close()
+
+			if testcase.txErr != nil {
+				mock.ExpectBegin().WillReturnError(testcase.txErr)
+			} else {
+				mock.ExpectBegin()
+				if testcase.queryErr != nil {
+					mock.ExpectQuery("SELECT id, topic, payload FROM events LIMIT 2 FOR UPDATE SKIP LOCKED").WillReturnError(testcase.queryErr)
+					mock.ExpectRollback()
+				} else {
+					if testcase.tName == "fail scan" {
+						mock.ExpectQuery("SELECT id, topic, payload FROM events LIMIT 2 FOR UPDATE SKIP LOCKED").WillReturnRows(mock.NewRows([]string{"id", "topic", "payload"}).AddRow("invalid id", topic, payload))
+					} else {
+						mock.ExpectQuery("SELECT id, topic, payload FROM events LIMIT 2 FOR UPDATE SKIP LOCKED").WillReturnRows(mock.NewRows([]string{"id", "topic", "payload"}).AddRow(id, topic, payload))
+						if testcase.deleteErr != nil {
+							mock.ExpectExec("DELETE FROM kafka_events where id IN \\('2'\\)").WillReturnError(testcase.deleteErr)
+							mock.ExpectRollback()
+						} else {
+							mock.ExpectExec("DELETE FROM kafka_events where id IN \\('2'\\)").WillReturnResult(sqlmock.NewResult(1, 1))
+							if testcase.kafkaErr != nil {
+								kafka.EXPECT().SendMessages(gomock.Any()).Return(testcase.kafkaErr)
+								mock.ExpectRollback()
+							} else {
+								kafka.EXPECT().SendMessages(gomock.Any()).Return(nil)
+								if testcase.commitErr != nil {
+									mock.ExpectCommit().WillReturnError(testcase.commitErr)
+								} else {
+									mock.ExpectCommit()
+								}
+							}
+						}
+					}
+				}
+			}
+
+			consumer := pkg.KafkaConsumer{
+				DB:        db,
+				Kafka:     kafka,
+				Batch:     2,
+				WorkerNum: 1,
+			}
+
+			err = consumer.Process()
+			if (testcase.outputErr == nil && err != nil) ||
+				(testcase.outputErr != nil && err == nil) ||
+				(err != nil && testcase.outputErr.Error() != err.Error()) {
+				t.Errorf("Test %s, err is %v, should be %v", testcase.tName, err, testcase.outputErr)
+			}
+
+			consumer.Shutdown()
+			consumer.Run()
+
+			if err := mock.ExpectationsWereMet(); err != nil {
+				panic(err)
+			}
+		})
 	}
 }
